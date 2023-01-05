e5fb3a043b3fe5f5727303ab074663040a2532c1
otabek <otabekhoshimxon@gmail.com>
2023-01-05 21:13:48 +0500

    Created

---
+++ src/main/java/uz/linuxuzbekistan/AppLinuxUzbekistanApplication.java
@@ -0,0 +1,13 @@
+package uz.linuxuzbekistan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLinuxUzbekistanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppLinuxUzbekistanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/linuxuzbekistan/AppLinuxUzbekistanApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.linuxuzbekistan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLinuxUzbekistanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
